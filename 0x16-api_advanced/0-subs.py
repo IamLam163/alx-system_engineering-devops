9dc9722cd0f74aab241a267a7981191fcf154034
IamLam163 <olamidealiyu@gmail.com>
2023-01-31 13:16:14 +0100

    pycode

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,12 +9,11 @@ def number_of_subscribers(subreddit):
     """Function returns number of subscribers"""
     URL = "https://api.reddit.com"
     headers = {
-            "User-Agent": "ChangeMeClient/0.1 by Olamide"
-            }
+        "User-Agent": "ChangeMeClient/0.1 by Olamide"
+    }
     req = requests.get("{}/r/{}/about".format(URL, subreddit),
                        headers=headers)
 
     if req.status_code == 404:
         return 0
     return req.json().get('data').get('subscribers')
-

b8c9afed64aba24a47cfd117aa68a39f16063025
IamLam163 <olamidealiyu@gmail.com>
2023-01-31 14:40:11 +0100

    top ten

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Function queried Reddit api and prints the titles
+of the first 10 hot posts"""
+
+import requests
+
+
+def top_ten(subreddit):
+    """returns first 10 posts"""
+    URL = "https://api.reddit.com"
+    headers = {"User-Agent": "ChangeMyClient/0.1 by Olamide"}
+    params = {"limit": 10}
+        
+    req = requests.get("{}/r/{}".format(URL, subreddit),
+                           headers=headers, params=params)
+    if req.status_code == 404:
+        return print(None)
+    children = req.json().get('data').get('children')
+    print("\n".join([child.get('data').get('title') for child in children]))
+
+
+

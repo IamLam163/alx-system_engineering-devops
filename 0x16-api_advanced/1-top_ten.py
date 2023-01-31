286dae1412907dbb1fb7f5612ebd4b3bc82dcdda
IamLam163 <olamidealiyu@gmail.com>
2023-01-31 14:40:59 +0100

    pycode

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -10,13 +10,10 @@ def top_ten(subreddit):
     URL = "https://api.reddit.com"
     headers = {"User-Agent": "ChangeMyClient/0.1 by Olamide"}
     params = {"limit": 10}
-        
+
     req = requests.get("{}/r/{}".format(URL, subreddit),
-                           headers=headers, params=params)
+                       headers=headers, params=params)
     if req.status_code == 404:
         return print(None)
     children = req.json().get('data').get('children')
     print("\n".join([child.get('data').get('title') for child in children]))
-
-
-
